72270cdfe8941d8abda3d2ab2300e63e760e1820
Łukasz Babut <admin@lukaszbabut.info>
2022-02-20 10:18:51 +0000

    Added HOWTO.pl

---
+++ users/LukaszBabut/MyAspieWorld/HOWTO.pl
@@ -0,0 +1,271 @@
+Życie wśród małp
+	Tak, tak. Przysżło Ci żyć wśród osobników obcego gatunku. Nie będzie łatwo.
+Ale spróbuję dać Ci kilka wskazówek, dzięki którym osiągnąłem znośny poziom niezadowolenia.
+1)	To oni są „Ci inni”.
+2)	To Ty jesteś w tym wszystkim jedyną rzeczą, która się liczy.
+3)	Małpy nie wiedzą, że są głupie i nie są w stanie tego zrozumieć. 
+4)  Nie udawaj, że Ci przykro, jak skrzywdzisz małpę. Nie jest tego warta.
+5)  It's ok to lie to a monkey.
+Tresura
+    jakie niegrzeczne dziecko. zupełnie nie umie się zachować. A powiedz wierszyk.....
+    Małpy od zawsze próbowały zrobić z nas inne małpy. Nic innego nie potrafią.
+    Średnio szło, więc zaczeły nas tresować.
+    Zmuszać.
+    Bić, katować, maltretować. Jak w cyrku.
+    Aż straumatyzowani zaczęliśmy robić im te sztuczki.
+    Jebac ich!
+Rodzice
+	
+	To całkiem przejebany temat. Trudny, bolesny i jeśli nie masz wewnętrznego przymusu zmierzenia się z nim, po prostu przestań ją traktować poważnie.
+Widzę jednak, że czytasz dalej. Też bym tak zrobił.
+	No więc zacznijmy od próby opisu sytuacji:
+Osoba, która miała Cię przygotować do życia na świecie okazała się głupsza od Ciebie, jak miałeś jakieś 5 lat.
+Zawiodły wszelkie próby komunikacji, więc wpadłeś na pomysł udawania czegoś innego przed nią - tak było łatwiej.
+W efekcie nie masz zatem pojęcia ani kim jesteś ani jak żyć wśród małp a ona ma cię za wariata.
+Na to wszystko nakładają się uczucia. Twoje uczucia. Jej uczucia...
+Tak, masz uczucia. Tylko boisz się używać.
+Ale nie takie, o których czytałeś wiersze, chyba, że przejebałes się przes Sylwię Plath czy coś jej kalibru.
+Musiałbyś to jednak zrobić w wieku 3 lat, kiedy małpy uczą się takich rzeczy. 
+A w zasadzie to oni musieliby to zrobić, zanim zaczęli Cię kochać po małpiemu - co i tak by nic nie dało.
+Ostrzegałem, że trudny temat.
+Dom
+   znajdź sobie miejsce, w którym nic nie przeszkodzi ci w byciu sobą.
+   Słuchanie tej samej piosenki przez całą dobę, ma tam być równie normalne, jak walenie konia.
+   jeśli to niemożliwe, zacznij szukać rozwiązania od nowa.
+   To jest sprawa najważniejsza.
+Oni patrzą!
+    I chuj. Jak pies/kot/mrówka na Ciebie patrzy, to też uważasz, że musisz się zachowywać stosownie? Bo może jej będzie przykro? LOL!!!
+    Przemyśl, jakie to głupie.
+    Kto i kiedy kazał Ci tak robić?
+    Po co?
+    wiem, że tak naprawdę masz w dupie czy małpie jest przykro - to tresura każe Ci brać to pod uwagę.
+Czy robię to dobrze?
+    zasadniczo tak. Who cares?
+Nowe sytuacje
+     Przygotuj się najlepiej, jak to możliwe. Zapisz mapy, trasy, godziny odjazdów, przyjazdów.
+     Obejrzyj okolicę w streetwiew.
+     Będzie mniej straszna
+     W uszy słuchawki - trochę odstraszają obce małpy od prób powiedzenia ci czegoś nieprzydatnego.
+Co odróżnia nas od małp
+     Czujemy więcej.
+     Myślimy lepiej.
+     Mamy inne uczucia.
+     
+Wkurwiacze
+    bodźce sensoryczne są super! Jak narkotyki. Przedawkujesz, nie balujesz.
+    Unikaj przedawkowania.
+    To aż tak proste.
+    Zawsze i wszędzie dbaj o swój komfort. Dokładnie tak, jak lubisz.
+    Małpy tego nie robią, bo nie czują [? chuj je w sumie wie czemu]
+    Serio jest Ci "głupio" zdjąć kurtkę w sklepie? No bo "jak to wygląda" :-) 
+    Bodźcem sensorycznym jest też stres - unikaj go. Nie mścij sie na własym ciele za głupotę otaczających cie małp!
+Rozpraszacze
+    miej notes pod ręką. Notuj, co i kiedy ci rozjebało proces myśłowy. 
+    Eliminuj przyczyny. Dbaj o kompfort. Nawet jeśli to oznaczam ,że małpy będą się śmiały.
+    To jest twój świat. Nie małpi.
+    Zazwyczaj pomaga zmiana ustawień telefonu/komputera/biurka...
+Psychotesty
+   przypuszczam, że zanim tu trafiłeś, zrobiłeś jeden z tych durnych testów prychlogicznych dla małp?
+weśmy jakiś randomowy "na warsztat". Ot na przykład ten:  https://www.diagnostyka-umyslu.pl/spektrum-autyzmu-test
+1) Proszę wybrać swoją płeć: [ powiedzmy, że to jeszcze wybaczam... ]
+2) Jak często masz trudności z uporządkowaniem rzeczy, gdy musisz wykonać zadanie wymagające organizacji?
+    [co to kurwa znaczy "trudności z uporządkowaniem rzeczy"?!?!?]
+    [co to jest "zadanie wymagające organizacji???]
+3) Jak często masz problemy z zapamiętywaniem terminów lub obowiązków?
+    [ po chuj mam to pamiętać?!?! Mam smartfona w kieszeni]
+4) Kiedy masz zadanie, które wymaga dużo przemyśleń, jak często unikasz lub opóźniasz rozpoczęcie?
+    [ to są jakieś zadania niewymagające przemyśleń ??]
+5) Jak często „wiercisz się” (poruszasz rękami lub stopami), kiedy musisz długo siedzieć?
+    [ ciągle, przeszkadza ci to? To spierdalaj :-) ]
+6) Jak często popełniasz nieostrożne błędy, gdy musisz pracować nad nudnym lub trudnym zadaniem?
+7) Jak często masz trudności z utrzymaniem uwagi, gdy wykonujesz nudną lub powtarzalną pracę?
+    [ ciągle, bo to są kurwa nudne zadania - ja jestem stworzony do wyższych rzeczy.
+    Jak mam nudny proces, to go kurwa automatyzuję i o nim zapominam. ]
+8) Jak często masz trudności ze skupieniem się na tym, co ludzie do Ciebie mówią, nawet jeśli zwracają się do Ciebie bezpośrednio?
+    [ ale co mówią? Pewnie pierdolą coś bez sensu - jak to małpy. Who cares!]
+9) Jak często się gubisz lub masz trudności ze znalezieniem rzeczy w domu lub w pracy?
+    [ małpy też by się pogubiły w mrowisku i chuja wiedziały, gdzie co leży.
+      no i kto wam miasta projektuje wy pojeby? :-) ]
+10) Jak często rozprasza Cię aktywność lub hałas wokół Ciebie?
+    [ spróbujcie kurwa odczuwać hałas tak, jak my, to pogadamy.
+       a tobie by przeszkadzał trzymetrowy chuj w dupie w bieganiu? 
+       Ja pierdole! ]
+11) Jak często czujesz się niespokojny lub niecierpliwy?
+    [ w śród małp? zawsze ;-) ]
+12) Jak często masz trudności z odprężeniem się i relaksem, gdy masz czas dla siebie?
+    [ "czas dla siebie" - lol. One myślą, że mamy takie ficzery.
+      nasze rozumienie relaksu i odprężenia też się ma nijak to małpiej wersji.
+      To jak rozmawiać ze świnią o atronomii ]
+13) Jak często przerywasz innym, kiedy są zajęci?
+    [ A po czym się poznaje, że małpa jest zajęta?
+    to działa w obie strony - my umiemy jednocześnie:
+        - doskonale udawać małpę
+        - robić kilka rzeczy
+        - myśleć o całkiem innych
+        - odpowiadać na wasze pojebane pytania
+        - tłumaczyć z naszego na małpi
+        - słuchać, wąchać, widzieć,
+        - nie ruszać się 
+        Małpa kiedy widzi jak siedzimy i się bujamy na krześle, to myśli, że nic nie robimy i że to jest OK, po
+        prostu podejść i zacząć gadać coś po małmiemu.
+        LOL - serio!]
+14) Wolę robić coś razem z innymi niż samemu.
+    [ no kurwa, raczej :-) 
+      małpy mają o to jakiś ból dupy, czy coś?]
+ 15) Wolę wykonywać czynności zawsze w ten sam sposób.
+    [ jeśli już poświęcam energię i używam ciała, do wykonania zmian w otoczeniu materialnym, to raczej wszysto uprzednio przemyślałem i robię to metodą, jaka mi pasuje.
+       Metodą idealną.
+       Odpierdolcie się :-)]
+Znudziło mi się z tym tstem - just hope you're getting the point now.
+Przemęczenie
+    mamy też wady - nasze świadomości zostały umieszczone w ciałach, które nie do końca nadają się do potrzeb.
+    Sposób w jaki funkcjonujemy wykańcza mózg. Zużywamy neuroprzekażniki, jak ruski czogł ropę.
+    Patrz: bipolar dosorder, depresja, etc.
+    Zwyczajnie zajeżdzamy sprzęt - pewnie przez ciagle działający emulator małpy. Ale to tylko teoria.
+    Nadal mam z tym problem i szukam rozwiązań.
+Wspomnienia i pamiętać
+    ileż to razy przeżywałeś w myślach sceny, w których dałeś dupy jako małpa? 
+    okurwa.. ale się zbłaźniłem na imprezie/w pracy/ szkole....
+    Lol... to twój emulator małpy się zbłaźnił. Nie może być doskonały - to tylko emulator.
+    Zbudowałeś go zgadująć "czego byćmoże doświadcza małpa w tej sytuacji" - powinieneś byc dumny z osiągnięcia, uznać je za ukończone i się nim znudzić.
+    Masz o czym myśleć :-)
+Socializing
+    nie, nie będzie o spotykaniu się z małpami.
+    Jest nas mnóstwo. Mamy internet. Możemy miec ze sobą relacje na zupełnie nowym, nieznamym w hostori poziomie i wymiarach.
+    Budować więzi, które nie mają małpiej nazwy.
+    Mieć do siebie nawzajem uczucia, które nie mają małpiej nazwy.
+    Mieć poetów naszego języka. 
+    Możemy wszystko, co chcemy.
+Maska
+    Zdejm jom. Nie bąć małpom.
+    Czuj, myśl, twórz, żyj! 
+        
+Lęk
+    każdy by sie bał, żyjąc w ten sposób.
+    Przestań udawać małpę.
+Dysonand poznawczy
+    przeczytałem gdzieś, że "autystyk musi być przygotowany i nie wolno mu przeszkadzać'... serio? A jak przestanie udawać małpę?
+    
+I chuj :-)
+A teraz dodaj cos od siebie. 
+Skomituj.
+Podziel się z nami.
+Jesteś w domu.
+
+introductinToTheMonkey.txt
+-40
+/HOWTOs/introductinToTheMonkey.txt
+View
+To ochydne, ale czasem niezbedne
+Na początek:
+- małpy uważają za istotne, jakie wywrzesz na nich "pierwsze wrażenie" - przemyśl swoje cele i dostosuj rytułał.
+- jest prawie niemożliwyn nadpisanie "pierwszego wrażenia" u małp.
+- ważne, zebyś unial to robić dobrze, więc ćwicz często
+Niestety jest wiele opcji i wariantów "powita". Zdążyłem przeanalizować tylko te:
+Zupełnie obca malpa + nie zostałeś przedstawiony
+1) Podejdź do małpy
+2) Popatrz jej przez chwilę w oczy [jakbyś widział coś ciekawego, interesującego ]
+3) Obczaj, co robi z ręką - kto wyciąga pierwszy jest dla mnie nadal tajemnicą.
+   Przypuszczam, że to sposób okazywania "władzy" u małp. Chcesz dominować = wyciągasz pierwszy
+4) Kontakt fizyczny 
+    [tak, wiem... obrzydliwe! Ale one jakoś tego "wymagają" ]
+    No więc najtrudniejsza część: poćwicz przed lustrem - mogą zauważyć obrzydzenia i całe przywitanie w pidu
+Małpa, którą dobrze znasz
+- tak Ci się wydaje... to nie takie proste. Małpy są tu nieprzewidywalne
+- nie ufaj im a jednocześnie nie pozwól tego po sobie poznać..
+Dobra, doczytaleś aź dotąd, więc przestanę pierdolić i przejdę do konkretów. To są świnie. Zwierzęta. Masz ich uzywać.
+Zacznij naukę od tego: https://en.wikipedia.org/wiki/Neuro-linguistic_programming
+Resztę znajdziesz w necie. Internet zrobili tacy jak my. Wciąż ma zasoby i ziomków.
+Tylko coraz trudniej ich znaleśź.
+wierzm, ze sobie poradzisz.
+
+JournetToNewMonkeyDesignedPlace
+-108
+/HOWTOs/JournetToNewMonkeyDesignedPlace
+View
+Małpie miasto
+     poniższe przyda ci się tylko przed pozbyciem się małpiego warunkowania (tresury)
+1) Przygotuj się
+    - zdrób kopię danych z głowy (musi być fizyczna - najlepsza ołówkiem w ulubionym notesie) o:
+       =  skąd, dokąd
+       = kiedy
+    = mapa skąd 
+        = mapa do kąd
+            = zapadowe połącznie
+            ---([ całość jest z grubsza faktalna, ćwicz dużo. Zwiedzaj. WARTO!] )
+2) Odizoluj się
+    - odzienie szaleńca ( ale "nie zabardzo" - żeby brały Cię za outsidera, ale wciąż z własngo stad
+    - słuchawki w uszy (migające wchuj!) do unikania kontantów werbalnych-audio 
+    
+    
+3) Na miejscu
+    Najwaźniejsze, to zachować spokój. 
+    Podtrzymać funkcje ciała ( oddychanie głównie najważniejsze )
+    Jak zrobisz powyższe, to instrukcje przestaną być potrzebne.
+    
+4) Zdobywaj 
+   nieżadko mi się zdażało pozytywne zaskoczenie. Warto obejrzeć małpie gaje. Warte uwagi są na przykład sposoby, na które próbują realizować architekturę ;-)
+    > fer: pozwolilo nawet jednemu z nas na zbudowaniw kilku rzeczy: https://en.wikipedia.org/wiki/Antoni_Gaud%C3%AD
+   Planeta też warta uwagi. Zwłaszcza [patrz, gdzie umarłem]
+xxx) Lista itemów, które warto rozważyć [TODO cleanup]
+Typ		Opis	Ile	lok									
+0gólne	x	Irenka	ok			Wyjazd:	4/30/2013	13:10	13:30	0:20			
+0gólne	x	podlać kwiatki	ok										
+Kosmetyczka		fajki		mały plecak	noce:	7							2
+Kosmetyczka	x	Chujsteczki Do noska	ok										
+Kosmetyczka	x	kosmetyczka !! Maszynka itepe + bloker !!!	rano										
+Kosmetyczka	x	Leki 											
+Kosmetyczka	x	okulary p. słoneczne, kontaktowe											
+Kosmetyczka	x	Kapcie po hotelu											
+Papiery	x	paszport, wyjazdowe, kasa											
+Sprzęt	x	roaming	ok										
+Sprzęt	x	telefony, aparat, ładowarki, akumulatorki, latarka		mały plecak									
+Sprzęt	x	Coś do wpierdalania w samolocie	ok										
+Survival	x	kawa, cukier, żarcie	ok										
+Survival	x	kołdra, poduszki	ok										
+Survival	x	Świeczki	ok										
+Ubrania	x	Adidaski	ok										
+Ubrania		Bluzy ciepłe	rano										
+Ubrania		Buty											
+Ubrania		Buty do sniegowania / latania	rano										
+Ubrania	x	czapka	rano										
+Ubrania	x	Dresikl	ok										
+Ubrania	x	Kapielówki	ok										
+Ubrania	x	Klapki prysznoicowe	ok										
+Ubrania	x	Koszulki	ok										
+Ubrania	x	Kurtka zimowa	ok										
+Ubrania	x	Majty	ok										
+Ubrania	x	Ręcznik	ok										
+Ubrania	x	rękawiczki i szalik	ok										
+Ubrania	x	Skarpetki	ok										
+Ubrania	x	Spodnie dł	ok										
+Ubrania	x	Sprodnie kr.											
+Ubrania	x	miętówki	ok										
+Ubrania	x	Sztormiak 	rano	mały plecak									
+Ubrania	x	T-szerty	ok										
+papiery	x	Glajtowe papiery	ok										
+Ubrania	x	maska z rurkąn do pływania	ok										
+	x	śpiwór											
+	x	Ubezpieczenie !!!											
+x		Do paznokci											
+		Lapki, zasilacze											
+		słuchawki											
+		gajerek											
+		buty biznesowe i ciupciarka buciana											
+		koszule krawaty											
+		pasek biznesowy											
+		Taksówka na rano !!											
+													
+		Piersióweczka											
+													
+		- ciepła bluza\polar											
+		- czapka i rękawiczki											
+		- ubranie przeciwdeszczowe, wiatrochronne											
+		- wygodne buty, najlepiej za kostkę lub inne trekkingowe											
+		- ciepłe skarpety											
+													
+		- termos na gorącą herbatę											
+		- bidon na wodę lub po prostu woda											
+		- czekolada bądź substytut kaloryczno-energetyczny											
+		- apteczka (bez szaleństw, ale warto mieć bandaż, plaster, folię NRC)											
+		- piersióweczka (bo toast za zdobyty szczyt być powinien)											
